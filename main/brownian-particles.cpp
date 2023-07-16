b9080ba50684d317144b3c7eee2b04ffc8222f61
alexl <guslechal@student.gu.se>
2023-07-16 18:54:06 +0200

    Further attempts at solving the issue, put on ice for now

--- main/brownian-particles.cpp
+++ main/brownian-particles.cpp
@@ -11,21 +11,18 @@ const double pi			    = 3.14159265358979323846;
 const double temperature            = 300;
 const double particleRadius         = 1*pow(10,-6); //1 micrometer sized particle
 const double cutoff_distance	    = 8*pow(10,-6);
-const double delta_t		    = 0.01;
-const double kb      		    = 1.38 * pow(10,-23);
+const double delta_t		    = 0.002;
+const double kb      		    = 1.380649 * pow(10,-23);
 const double viscosity              = 1*pow(10,-3);
 const double D_T 	            = kb *temperature / (6.0*pi*viscosity*particleRadius);
-const double D_R	   	    = 1;
-const double v			    = 2*pow(10,-6);  
 const double v_0		    = 10*pow(10,-6); //Phoretic strength
 const double box_size		    = pow(10,-4);
 const double phoreticTerm	    = v_0 * pow(2*particleRadius,2);
 const double trans 		    = sqrt(2.0*D_T*delta_t);
-const double rotate 		    = sqrt(2.0*D_R*delta_t);
 
-
-const double spawnArea		    = box_size/20;
-
+const double v			    = particleRadius*10;
+const double spawnArea		    = box_size/30;
+const double cDR  		    = sqrt(2*kb*temperature/(8*pi*0.001*pow(particleRadius,3))*delta_t);
 
 
 
@@ -39,12 +36,11 @@ struct Particle{
 	//Coords
 	double x;
 	double y;
-	
+	double phi;
 	//Phoretic velocity directions;
 	bool isHot;
 	double vpx; 
 	double vpy;
-	double phi;
 }; 
 
 
@@ -54,8 +50,8 @@ void update_position(Particle &particle);
 std::vector<double> phoretic_force(Particle &particle1, Particle &particle2);
 void hard_sphere_correction(std::vector<Particle>& particles,int nParticles);
 
-vector<double> getDirection(Particle particle1, Particle particle2);
-double getNorm(Particle particle1, Particle particle2);
+vector<double> getDirection(Particle &particle1, Particle &particle2);
+double getNorm(Particle &particle1, Particle &particle2);
 
 void boundary_box_correction(std::vector<Particle>& particles,int nParticles);
 void writeToCSV(std::vector<std::vector<Particle>> particlesOverTime,int nParticles,int timeSteps);
@@ -85,47 +81,48 @@ int main(int argc, char** argv) {
 	vector<vector<Particle>> particlesOverTime;
 
    	
+   	//simulate over time:
 	for (int time = 0; time < timeSteps; time++){
-		hard_sphere_correction(particles,nParticles);
-		hard_sphere_correction(particles,nParticles);
-		hard_sphere_correction(particles,nParticles);
-		hard_sphere_correction(particles,nParticles);
+
 		
+		for (int i = 0; i<nParticles;i++){
+			update_position(particles[i]);
+		}
+		//calculate phoretic force	
 		for (int i = 0; i< nParticles; i++){
-			double vpx = 0.0;
-			double vpy = 0.0;
 		
+			double vpx = 0.0;
+   			double vpy = 0.0;
+   			
 			for(int j = 0; j< nParticles; j++){
-				if (i==j) {continue;}
-				//Only attracted to hot particles, and not to itself.
-				if (particles[j].isHot == true){
+
+				if (particles[j].isHot == true && i != j){
 					vector<double> totalPhoreticForce = phoretic_force(particles[i],particles[j]);
 					vpx += totalPhoreticForce[0];
 					vpy += totalPhoreticForce[1];		
 				}
-			}
 			
-			particles[i].vpx = vpx;
-			particles[i].vpy = vpy;
-			update_position(particles[i]);
+			}
+
+			particles[i].vpx =  vpx;
+			particles[i].vpy =  vpy;	
 			
 		}
+		
+		
 		hard_sphere_correction(particles,nParticles);
 		hard_sphere_correction(particles,nParticles);
 		hard_sphere_correction(particles,nParticles);
-		hard_sphere_correction(particles,nParticles);
+		
 		boundary_box_correction(particles,nParticles);
-		hard_sphere_correction(particles,nParticles);
-		hard_sphere_correction(particles,nParticles);
-		hard_sphere_correction(particles,nParticles);
-		hard_sphere_correction(particles,nParticles);
-
 		particlesOverTime.push_back(particles);
 	
 		
 	}
 	writeToCSV(particlesOverTime,nParticles,timeSteps);
 
+
+	
    	auto end = std::chrono::high_resolution_clock::now();
    	std::chrono::duration<double> duration = end - start;
    	double elapsed_seconds = duration.count();
@@ -148,7 +145,6 @@ std::vector<Particle> initialize_particles(int nHot, int nCold) {
 		
 		if(i < nHot){
 			newParticle.isHot = true;
-		
 		}else{
 			newParticle.isHot = false;
 		}		
@@ -156,12 +152,10 @@ std::vector<Particle> initialize_particles(int nHot, int nCold) {
 		newParticle.y = dis(gen)*spawnArea + box_size/2;
 		newParticle.vpx = 0.0;
 		newParticle.vpy = 0.0;
-		newParticle.phi = dis(gen)*2.0*pi;
+		newParticle.phi = 0.0;
 		particles.push_back(newParticle);	
 		
 	}	
-	
-	
 	
 	return particles;
 }
@@ -169,14 +163,16 @@ std::vector<Particle> initialize_particles(int nHot, int nCold) {
 
 void update_position(Particle &particle) {
 
-	normal_distribution<double> normdis(0, 1);
-	double W_phi  = normdis(gen);
+	normal_distribution<double> normdis(0.0, 1.0);
 	double W_x    = normdis(gen);
 	double W_y    = normdis(gen);
-	double newPhi = particle.phi + W_phi*rotate;
+	double W_phi  = normdis(gen);
 	
-	particle.x = particle.x +  v*cos(newPhi) * delta_t + trans*W_x + particle.vpx*delta_t;
-	particle.y = particle.y +  v*sin(newPhi) * delta_t + trans*W_y + particle.vpy*delta_t;
+	particle.phi = particle.phi + W_phi*sqrt(2)*sqrt(delta_t);
+	
+	
+	particle.x = particle.x   + trans*W_x + particle.vpx*delta_t;
+	particle.y = particle.y   + trans*W_y + particle.vpy*delta_t;
 
 }
 
@@ -184,25 +180,28 @@ void update_position(Particle &particle) {
 void hard_sphere_correction(std::vector<Particle> &particles,int nParticles) {
 
 
-	for (int i = 0 ; i<nParticles-1; i++){
-		for(int j = i+1; j<nParticles; j++) {
-		
+	for (int i = 0; i<nParticles; i++){
+	
+		for(int j = i; j<nParticles; j++) {
 			double centerToCenterDistance =  getNorm(particles[i],particles[j]);
 			double overlap = 2.0*particleRadius - centerToCenterDistance;
 			
-			if (overlap > 0 && overlap != 2.0*particleRadius){
+			
+			if (overlap > 0 && i!=j){
 			
 				double distanceToMove = overlap/2.0;
 				vector<double> direction = getDirection(particles[i],particles[j]);
 				
+		
 				particles[i].x = particles[i].x + distanceToMove*direction[0];
 				particles[i].y = particles[i].y + distanceToMove*direction[1];	
-				
 				particles[j].x = particles[j].x - distanceToMove*direction[0];
 				particles[j].y = particles[j].y - distanceToMove*direction[1];	
 				
+			}
+		
 			
-			}						
+									
 		}
 	}
 }
@@ -214,13 +213,15 @@ std::vector<double> phoretic_force(Particle &particle1, Particle &particle2){
 	double vpx = 0.0;
 	double vpy = 0.0;
 	
-	double particleDistance = getNorm(particle1,particle2);
+	double particleDistance = getNorm(particle2,particle1);
 			
 	if (particleDistance < cutoff_distance && particleDistance !=0) {
+	
 		double directionx =  (particle2.x-particle1.x)/particleDistance;
 		double directiony =  (particle2.y-particle1.y)/particleDistance;
 		vpx = (phoreticTerm/(pow(particleDistance,2)))*directionx;
 		vpy = (phoreticTerm/(pow(particleDistance,2)))*directiony;
+		
 		
 		return {vpx,vpy};
 	} else {
@@ -230,20 +231,19 @@ std::vector<double> phoretic_force(Particle &particle1, Particle &particle2){
 }
 
 
-vector<double> getDirection(Particle particle1, Particle particle2){
+vector<double> getDirection(Particle &particle1, Particle &particle2){
 	//Return normalized direction, in x and y coordinates.
-	double norm = sqrt(pow(particle1.x-particle2.x,2) + pow(particle1.y-particle2.y,2));
+	double norm = getNorm(particle1,particle2);
 	double x = (particle1.x-particle2.x)/norm;
 	double y = (particle1.y-particle2.y)/norm;
 	
 	vector<double> direction = {x,y};
 
-	
 	return direction;
 }
 
 
-double getNorm(Particle particle1, Particle particle2){
+double getNorm(Particle &particle1, Particle &particle2){
 	double norm = sqrt(pow(particle1.x-particle2.x,2) + pow(particle1.y-particle2.y,2));
 	return norm;
 }
@@ -276,21 +276,20 @@ void writeToCSV(std::vector<std::vector<Particle>> particlesOverTime,int nPartic
    	coldFile << "x,y" << std::endl;
    	
     // Write particle positions
-    for (int t = 0;t<timeSteps; t++) {
-    	vector<Particle> particles = particlesOverTime[t];
+	for (int t = 0;t<timeSteps; t++) {
+    		vector<Particle> particles = particlesOverTime[t];
     
-    	for (const auto& particle : particles) {
+    		for (const auto& particle : particles) {
     
-  		if (particle.isHot == true){
-  			hotFile << particle.x << "," << particle.y << std::endl;
-  		}
-  		else
-  		{
-  			coldFile << particle.x << "," << particle.y << std::endl;
-  		}
-    	}	   
+  			if (particle.isHot == true){
+  				hotFile << particle.x << "," << particle.y << std::endl;
+  			}
+  			else{
+  				coldFile << particle.x << "," << particle.y << std::endl;
+  			}
+    		}	   
     
-    }
+   	}
     	hotFile.close();
    	coldFile.close();
 }
--- main/visualizerNEW.py
+++ main/visualizerNEW.py
@@ -2,14 +2,14 @@ import pandas as pd
 import subprocess
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+plotMarkerSize = 14
+plotDPI        = 250
 
-plotMarkerSize=16
-plotDPI = 250
 
-nHot = 3
-nCold = 4
+nHot       = 1
+nCold      = 2
 nParticles = nHot + nCold
-timeSteps = 310
+timeSteps  = 500
 #Handler to simulate in C++:
 subprocess.run(['g++','brownian-particles.cpp','-o','sim','-O4'])
 subprocess.run(['./sim',f' {nHot}',f' {nCold}',f' {timeSteps}'])
@@ -18,7 +18,7 @@ print("Processing data...")
 box_size = 100 * 1e-6
 
 # Read the CSV file into a DataFrame
-dfHot = pd.read_csv('hotParticles.csv', header=None, names=['x', 'y'],low_memory=False)
+dfHot  = pd.read_csv('hotParticles.csv', header=None, names=['x', 'y'],low_memory=False)
 dfCold = pd.read_csv('coldParticles.csv', header=None, names=['x', 'y'],low_memory=False)
 
 # Extract the x and y coordinates from the DataFrame
@@ -29,15 +29,14 @@ dfCold['x'] = pd.to_numeric(dfCold['x'],errors='coerce')
 dfCold['y'] = pd.to_numeric(dfCold['y'],errors='coerce')
 
 
-# Define the number of timesteps (100 rows per timestep)
-#NOTE THIS ASSUMES THAT THERE ARE EQUAL AMMOUNTS OF HOT AND COLD PARTICLES
 
 #%%
 # Create a figure and axis for the scatter plot
+
 fig, ax = plt.subplots(dpi=plotDPI)
 
 # Create an empty scatter plot
-scatterHot = ax.scatter([], [],s=plotMarkerSize,marker='o',color='red',label="Hot Particle")
+scatterHot  = ax.scatter([], [],s=plotMarkerSize,marker='o',color='red',label="Hot Particle")
 scatterCold = ax.scatter([], [],s=plotMarkerSize,marker='o',color='blue',label="Cold Particle")
 # Set the axis limits
 ax.set_xlim([0,box_size]) 
@@ -49,23 +48,25 @@ ax.set_ylabel('Y')
 
 # Function to update the scatter plot for each frame
 def update(frame):
+    #Limit the number of frames in the gif to reduce loading time
+   # if frame %3 !=0 and frame %3 != 1:
+    if frame %2 ==0:
+        startCold = frame * nCold
+        endCold   = startCold + nCold +1
+        startHot  = frame * nHot
+        endHot    = startHot + nHot+1
     
-    startCold = frame * nCold
-    endCold   = startCold + nCold
-
-    startHot  = frame * nHot
-    endHot    = startHot + nHot
-
-    scatterCold.set_offsets(dfCold.iloc[startCold:endCold, :].values)
-    scatterHot.set_offsets(dfHot.iloc[startHot:endHot, :].values)
+        scatterCold.set_offsets(dfCold.iloc[startCold:endCold, :].values)
+        scatterHot.set_offsets(dfHot.iloc[startHot:endHot, :].values)
     return scatterHot,scatterCold
 
 # Create the animation
-ani = animation.FuncAnimation(fig, update, frames=timeSteps, interval=10)
-
+ani = animation.FuncAnimation(fig, update, frames=timeSteps, interval=1)
+print("Encoding movie...")
 # Save the animation as a movie
 ani.save('particle_movie.gif', writer='pillow')
 
 # Show the plot (optional)
 gif_file = "particle_movie.gif"
+print("Encoding complete, opening movie.")
 subprocess.run(["xdg-open",gif_file])
