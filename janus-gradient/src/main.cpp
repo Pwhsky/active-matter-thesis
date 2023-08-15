a0395a4a4c4a96b61cc2c673d316c921c5072875
alexl <guslechal@student.gu.se>
2023-08-15 16:56:41 +0200

    Now get physical values

--- janus-gradient/src/main.cpp
+++ janus-gradient/src/main.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <fstream>
 #include <random>
+#include <omp.h>
 using namespace std;
 
 //Janus particle gradient computation:
@@ -14,24 +15,29 @@ std::mt19937 gen(rd());
 
 
 //Constants:
-
-
-	const long double particleRadius        = 2*pow(10,-6);
-
-	const long double particleRadiusSquared = pow(particleRadius,2);
 	const long double pi	       		= 3.14159265358979323846;
 	const long double twoPi			= 2*pi;
-	const long double bounds 		= particleRadius*5;
-	const long double Intensity		= 200*pow(10,-3); // milliwatt laser
+
+
+	//Particle:
+	const long double particleRadius        = 2*pow(10,-6);
+	const long double particleRadiusSquared = pow(particleRadius,2);
+	const int 	  nDeposits		= 6500;
+	const long double volumePerDeposit      = 4.1666*(10,-4)/nDeposits;
+	//const long double volumePerDeposit	= 4*pi*pow((100*pow(10,-9)),3)/3;
 	
-	const long double I0			= 2*Intensity/pow(bounds*2,2);
-	const long double alphaIron		= 2.43*pow(10,7);  //
-	const int 	  nDeposits		= 10;
+	
+	//Simulation box:
+	const long double bounds 		= particleRadius*20; //40 microns for now
 	const long double kWater 		= 0.598; //W/m·K
-	const long double volumePerDeposit      = 4.1666*(10,-4);
-	const long double lambda		= particleRadius;  //wavelength of laser.
-	long double stepSize;
-	long double dv;
+	      long double dv;
+	      long double stepSize;
+	      
+	//Laser:
+	const long double lambda		= 808*pow(10,-9);  //wavelength of laser. Keep above particleRadius generally
+	const long double Intensity		= 100*pow(10,-3); // milliwatt laser
+	const long double I0			= 2*Intensity/pow(bounds*2,2);
+
 	
 	
 struct Point{
@@ -43,182 +49,86 @@ struct Point{
 
 
 void writeToCSV(vector<long double> x,vector<long double> y,vector<long double> z,vector<vector<vector<long double>>> field);
+float fastSqrt(const float n);
 
 
-
-float fastSqrt(const float n) 
-{
-   static union{int i; float f;} u;
-   u.i = 0x5F375A86 - (*(int*)&n >> 1);
-   return (int(3) - n * u.f * u.f) * n * u.f * 0.5f;
-}
-
-long double getq(long double x) {
-	//Compute 
-	long double I = I0 * (1+ cos(  twoPi*x/lambda));
-	
-	long double output = I/(volumePerDeposit*kWater);
-	return output;
-	
-}	
-
-
-
-long double integral(long double x, long double y, long double z,vector<Point> deposits){
+inline long double integral(long double x, long double y, long double z,vector<Point> deposits){
 	long double contributionSum = 0.0;
-
-
+	long double q;
     	for (int i = 0; i < deposits.size(); i++){
-    	
-    		long double distance = sqrt((x-deposits[i].x)*(x-deposits[i].x) + (y-deposits[i].y)*(y-deposits[i].y) + (z-deposits[i].z)*(z-deposits[i].z));
+    		long double distance = sqrt((x-deposits[i].x)*(x-deposits[i].x) + 
+    					    (y-deposits[i].y)*(y-deposits[i].y) +
+    					    (z-deposits[i].z)*(z-deposits[i].z));
     		
-		long double q = getq(deposits[i].x)/distance;
+    		
+    		
+    		
+    		q = ((I0 + I0*cos(twoPi*distance/lambda))/volumePerDeposit)/(distance*kWater);
 		contributionSum -=  q;
 	}
-
-    	return dv*contributionSum;
-	
-	
+    	return contributionSum*dv;
 }
 
 
+inline void generateDeposits(vector<Point> &deposits) {
+
+//Places out the deposits evenly according to fibonacci sphere.
+	Point newPoint;
+	long double r,theta,phi;
+	phi = pi*(3.0 -sqrt(5.));
+	r = particleRadius;
+	for(int i = 0; i < nDeposits; i++){
+		newPoint.y = (1- pow((i/(nDeposits-1)),2)) *r;
+		long double tempRad = sqrt(1-newPoint.y*newPoint.y);
+		theta = phi*i;
+		newPoint.x = cos(theta) * tempRad*r;
+		newPoint.z = sin(theta) * tempRad*r;
+		
+		deposits.push_back(newPoint);	
+	}
+}
 
 
 
 int main(int argc, char** argv) {
 	auto startTimer = std::chrono::high_resolution_clock::now();
-	uniform_real_distribution<double> dis(0.0,1.0);
+
 	const long double resolution     = stof(argv[1]);
 	stepSize       			 = bounds/(resolution);
-	const long double coating        = stof(argv[2])*pi;
 	
-	dv	 = stepSize*stepSize*stepSize;
-
+	
+	dv = stepSize*stepSize*stepSize;
 	cout<<"step size = " <<stepSize<<endl;
 
-
-	//Generate coordinates for FeO deposits:
+//Generate coordinates for FeO deposits:
 	vector<Point> deposits;
-	Point newPoint;
-	long double r,theta,phi;
-	r = particleRadius;
-	for(int i = 0; i < nDeposits; i++){
-		theta = dis(gen)*2*pi;
-		phi = dis(gen)*pi;
-		
-		newPoint.x = r*sin(theta)*cos(phi);
-		newPoint.y = r*sin(theta)*cos(phi);
-		newPoint.z = r*cos(theta);
-		deposits.push_back(newPoint);
-		
-		
-	}
+	generateDeposits(deposits);
 	
 	
+//Initialize space in cartesian coordinates
+	vector<long double> spaceVector;
+	for (long double coordinate = -bounds; coordinate <= bounds; coordinate += stepSize) {
+        	spaceVector.push_back(coordinate);
+     	}
+     	const vector<long double> z = spaceVector;
+	const vector<long double> x = spaceVector;
+	const vector<long double> y = {0.0};
+     	 
+     	 
 	
-	
-	
-	
-	//Initialize space in cartesian coordinates
-	 vector<long double> z;
-	 for (long double coordinate = -bounds; coordinate <= bounds; coordinate += stepSize) {
-         	z.push_back(coordinate);
-     	 }
-	 vector<long double> y = {0.0};
-	
-	 vector<long double> x = z;
 	 vector<vector<vector<long double>>> field(x.size(), vector<vector<long double>>(y.size(), vector<long double>(z.size())));
  	 cout<<"Finished initialization"<<endl;
  	
-		
-		
-    		for (int i = 0; i<x.size(); i++){
-    			for(int j = 0; j<y.size(); j++){
-    				for(int k = 0; k<z.size(); k++){
-					//For a given point in 3D space, compute the integral given by Agnese
-					field[i][j][k] = integral(x[i],y[j],z[k],deposits);
-    				}
+ 	#pragma omp parallel for
+    	for (int i = 0; i<x.size(); i++){
+    		for(int j = 0; j<y.size(); j++){
+    			for(int k = 0; k<z.size(); k++){
+				//For a given point in 3D space, compute the integral given by Agnese
+				field[i][j][k] = integral(x[i],y[j],z[k],deposits);
     			}
     		}
-    	
-
- 
-    		
-    		
-
- 	
- 	
- 	
-
- 	
- 	float total_points = y.size()*z.size()*x.size();
- 	cout<<"total no. of points: "<<total_points<<endl;
- 	
-	
-	//Finite Difference Iterator/////////////
-	//Note, this takes alot of time..../////
-	/*
-	for (int t = 0; t < iterationLimit; t++) {
-   	 vector<Point> tempGradientList;
-
-
-
-		
-    		for ( auto point1 : gradientList) {
-    			Point updatedPoint = point1; 
-     	 		bool updated = false; 
-     	 		
-       		  	for ( auto point2 : gradientList) {
-            	   		if (point1.x != point2.x && point1.y != point2.y && point1.z != point2.z && point1.r >= particleRadiusSquared) {
-            	   		
-               				long double dx = (point1.x - point2.x);
-               				long double dy = (point1.y - point2.y); 
-                			long double dz = (point1.z - point2.z);
-               				const long double distance = fastSqrt(dx * dx + dy * dy + dz * dz);
-               				
-               				
-              				if (distance <= (stepSize)) {
-                   				updatedPoint.gradientValue +=(point2.gradientValue)/6;
-                   				updated = true;
-                    				break;
-               				}
-            			}
-       		  	}
-     			tempGradientList.push_back(updated ? updatedPoint : point1);
-     		}
-   		gradientList = move(tempGradientList);
-   		cout<<"finished iteration " << t<<endl;
-	}*/
-
-	////////////////////////////////////////////////
-
-		//The optimizer loves const datatypes, sorry for the clutter...
-		
-		/*
-		
-	for (long double y = 0.0; y<bounds; y += stepSize){
-		const long double iy = y;
-		for (long double x = -bounds; x<bounds; x += stepSize){
-			const long double ix = x;
-			for (long double z = -bounds; z<bounds; z += stepSize){
-				const long double iz = z;
-				
-				const long double distanceSquared = ix*ix+iy*iy+iz*iz; //Distance from origo
-
-				//Check if the point is inside the particle:
-				
-				if(distanceSquared>particleRadiusSquared){
-				
-					const long double sum = sumContributions(goldCoating,ix,iy,iz);
-					const long double gradientValue = 1/(sum*sum);		
-					appendValues(newPoint,gradientList,ix,iy,iz,gradientValue);
-				}
-			
-				
-			}
-		}
- 	}
-	*/
+    	}
+    
 	
 	cout<<"Simulation finished, writing to csv..."<<endl;
 	writeToCSV(x,y,z,field);
@@ -235,25 +145,56 @@ int main(int argc, char** argv) {
 }
 
 
+float fastSqrt(const float n) 
+{
+   static union{int i; float f;} u;
+   u.i = 0x5F375A86 - (*(int*)&n >> 1);
+   return (int(3) - n * u.f * u.f) * n * u.f * 0.5f;
+}
 
 
-void writeToCSV(vector<long double> x,vector<long double> y,vector<long double> z,vector<vector<vector<long double>>> field) {
+void writeToCSV(const vector<long double> x,const vector<long double> y, const vector<long double> z,vector<vector<vector<long double>>> field) {
 
 	ofstream outputFile("gradient.csv");
    	outputFile << "x,y,z,gradientValue" << std::endl;
+
    for (int i = 0; i <x.size(); i++){
 	for (int j = 0; j < y.size(); j++){
 			for (int k = 0; k < z.size(); k++){
-			
-	
-				
 				outputFile << x[i] << "," <<y[j] <<"," << z[k]<<"," << field[i][j][k] << std::endl;		
 			}
 		}
-	}
+    }
     	outputFile.close();
 
 }
 
+/*
+inline long double getq(long double x) {
+	//Compute 
+	//long double I = I0 * (1+ cos(  twoPi*x/lambda));
+	
+	
+	long double output = (I0 + I0*cos(twoPi*abs(x)/lambda))/volumePerDeposit/kWater;
+	return output;
+}*/
+
+/*
+inline void generateDeposits(vector<Point> &deposits) {
+
+	
+	Point newPoint;
+	long double r,theta,phi;
+	
+	for(int i = 0; i < nDeposits; i++){
+		theta = dis(gen)*2*pi;
+		phi   = dis(gen)*pi;
+		r = particleRadius*dis(gen);
+		newPoint.x = r*sin(theta)*cos(phi);
+		newPoint.y = r*sin(theta)*sin(phi);
+		newPoint.z = r*cos(theta);
+		deposits.push_back(newPoint);	
+	}
+}*/
 
 
--- janus-gradient/src/main.py
+++ janus-gradient/src/main.py
@@ -4,17 +4,15 @@ import pandas as pd
 import time 
 import subprocess
 import sys
+from matplotlib_scalebar.scalebar import ScaleBar
 
-
-resolution = sys.argv[1] #4000 = 151.648 seconds
+#resolution = sys.argv[1] #4000 = 151.648 seconds
 #coating    = sys.argv[2]
-
-#resolution = "50"
-coating =    "0.5"
+resolution = "300" #100 is fast, 500 is slow loading.
 
 
-subprocess.run(["g++","main.cpp","-o","sim","-O4"])
-subprocess.run(["./sim",resolution,coating])
+subprocess.run(["g++","main.cpp","-o","sim","-O4", "-fopenmp"])
+subprocess.run(["./sim",resolution])
 
 #%%
 #df = np.genfromtxt('gradient.csv',delimiter=',',skip_header=1)
@@ -22,7 +20,7 @@ df = pd.read_csv('gradient.csv')
 df.sort_values(by=['y'])
 
 
-scale = 6e-6
+scale = 10e-6
 #Extract the data from the DataFrame
 
 x = df['x']
@@ -50,24 +48,31 @@ ax = fig.add_subplot(111)
 # cmap='viridis' is used to map colors to a colormap (you can change it as needed)
 
 
-#sc = ax.scatter(df["x"],df["z"],c=df["gradientValue"], cmap='plasma', marker='.',s=130)
-sc = ax.scatter(Fsum["x"],Fsum["z"],c=F, cmap='plasma', marker='.',s=130)
+sc = ax.scatter(df["x"],df["z"],c=df["gradientValue"], cmap='plasma', marker='.',s=130)
+#sc = ax.scatter(Fsum["x"],Fsum["z"],c=F, cmap='plasma', marker='.',s=130)
 
 #sc = ax.scatter(x,y,z,c=df["gradientValue"], cmap='plasma', marker='.',s=100)
 
-cbar = plt.colorbar(sc)
-ax.set_xlabel('X')
 
-ax.set_ylabel('Z')
+cbar = plt.colorbar(sc,label ="$\Delta T$" )
+ax.set_xlabel('X $\mu m$')
+
+ax.set_ylabel('Z $\mu m$')
+
+
 
 ax.set_facecolor('black')
-ax.set_yticklabels([])
-ax.set_xticklabels([])
+
+ax.set_yticks([min(z),min(z)/2,0,max(z)/2,max(z)])
+ax.set_yticklabels([-20,-10,0,10,20])
+
+ax.set_xticks([min(x),min(x)/2,0,max(x)/2,max(x)])
+ax.set_xticklabels([-20,-10,0,10,20])
 
 
+ax.set_xlim(-scale,scale)    #For slice representation
+ax.set_ylim(-scale,scale) #For cube representation
 
-#ax.set_xlim(-scale,scale)    #For slice representation
-#ax.set_ylim(-scale,scale) #For cube representation
 #ax.set_ylim(0,0)
 #ax.set_zlim(-scale,scale)
 # Show the plot
