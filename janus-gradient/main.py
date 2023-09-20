5241b7d2d7f2a8c1eddfafc93e18852279e58d1c
alexl <guslechal@student.gu.se>
2023-09-20 17:20:13 +0200

    Now creates 3 figures, added laser profile as well

--- janus-gradient/main.py
+++ janus-gradient/main.py
@@ -10,6 +10,11 @@ from matplotlib.patches import Circle
 from cython_functions import histogram2d_cython, gradient_cython
 
 pi = 3.14159
+circle1 = Circle((0, 0), 2e-6)
+circle1.set(fill=False, linestyle='--', alpha=0.2)
+circle2 = Circle((0,0), 2e-6)
+circle2.set(fill=False,linestyle='--',alpha=0.2)
+
 imageBounds 	   = float(sys.argv[4])*1e-6
 spatialPeriodicity = float(sys.argv[5])*1e-9
 
@@ -17,12 +22,9 @@ def dfToNumpy(column):
     return column.to_numpy()
 
 def parseArgs():
-	
-
 	generateData    	 = str(sys.argv[3])
 	resolution 	         = sys.argv[1] 
 	nDeposits                = str(sys.argv[2])
-
 	return resolution,nDeposits,generateData
 	
 def generateNewData():
@@ -42,65 +44,72 @@ def loadData():
         	executor.submit(dfToNumpy, df['y']),
       		executor.submit(dfToNumpy, df['z']),
        		executor.submit(dfToNumpy, df['gradientValue'])
-    	]
+       		]
 	x    = futures[0].result()
 	y    = futures[1].result()
 	z    = futures[2].result()
 	grad = futures[3].result()
 	return x,y,z,grad,depositDF
-def generateLaserProfile(spatialPeriodicity):
-
-	x = np.linspace(-imageBounds,imageBounds, 200)
-	y = np.linspace(-imageBounds,imageBounds, 200)
-	X,Y = np.meshgrid(x,y)	
-	Z = 1+ (np.cos(2*pi*X/spatialPeriodicity))
-	return X,Y,Z
 	
+def generateLaserProfile(spatialPeriodicity):
+	x = np.linspace(-imageBounds*2,imageBounds*2, 200)
+	y = np.linspace(-imageBounds*2,imageBounds*2, 200)
+	X,Y = np.meshgrid(x,y)	
+	Z = (1+ (np.cos(2*pi*X/spatialPeriodicity)))/2
+	return X,Y,Z
 	
 def generateFigure(imageBounds):
 	fig, ax = plt.subplots(1, 3, figsize=(21, 5))
+	axisTitles = [f"$\Delta$T for {nDeposits} deposits, $\Lambda$ = {sys.argv[5]}nm",f"Position of {nDeposits} deposits",f"Laser intensity for $\Lambda$ = {sys.argv[5]} nm"] 
+	axisLabelsX = ['X ($\mu m$)','X ($\mu m$)','X ($\mu m$)']
+	axisLabelsY = ['Z ($\mu m$)','Z ($\mu m$)','Y ($\mu m$)']
+	circles	    = [circle1,circle2]
+	
+	index = 0
 	for axis in ax:
+		axis.set_title(axisTitles[index])
 		axis.axis('equal')
 		axis.set_xlim(-imageBounds,imageBounds)
 		axis.set_ylim(-imageBounds,imageBounds)
-		axis.set_xlabel('X $\mu m$')
-		axis.set_ylabel('Z $\mu m$')
+		
+		axis.set_xlabel(axisLabelsX[index])
+		axis.set_ylabel(axisLabelsY[index])
 		
 		axis.set_yticks([min(z),min(z)/2,0,max(z)/2,max(z)])
 		axis.set_xticks([min(x),min(x)/2,0,max(x)/2,max(x)])
+		
 		axis.set_xticklabels([round(min(x)*1e6),round(min(x)/2*1e6),0,round(max(x)/2*1e6), round(max(x)*1e6)])
 		axis.set_yticklabels([round(min(z)*1e6),round(min(z)/2*1e6),0,round(max(z)/2*1e6), round(max(z)*1e6)])
-	
-	ax[2].set_ylabel('Y $\mu m$')	
-	ax[1].grid(True)	
-	circle2 = Circle((0, 0), 2e-6)
-	circle2.set(fill=False, linestyle='--', alpha=0.2)
-	ax[1].scatter(depositDF['x'], depositDF['z'], label='_nolegend_',s=10)
-	ax[1].add_patch(circle2)
+		if index == 0:
+			axis.add_patch(circles[index])
+			im = ax[0].imshow(H.T, origin='lower',  cmap='plasma',
+           			 extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]])
+			cbar = plt.colorbar(im,ax=ax[0])
+			cbar.set_label(f"$\Delta$T [K]")
+			axis.add_patch(circles[index])
+			
+		if index == 1:
+			ax[1].grid(True)	
+			ax[1].scatter(depositDF['x'], depositDF['z'], label='_nolegend_',s=10)
+			axis.add_patch(circles[index])
+		if index == 2:
+			X,Y,Z = generateLaserProfile(spatialPeriodicity)
+			laserImage = ax[2].contourf(X,Y,Z,50)
+			cbar2 = plt.colorbar(laserImage,ax=ax[2])
+			cbar2.set_label(" I(x) / $\mathrm{I}_0$")
+			
+		
+		index+=1
 
-	x_bins = np.linspace(-imageBounds,imageBounds,200)
-	y_bins = np.linspace(-imageBounds,imageBounds,200)
-	H, xedges, yedges = histogram2d_cython(x, z, grad, x_bins, y_bins)
-
-	#Add circle
-	circle = Circle((0,0), 2e-6)
-	circle.set(fill=False,linestyle='--',alpha=0.2)
-	ax[0].add_patch(circle)
-	
 	#Labels & Legend	
 	fig.legend([ "Particle boundary"],loc='lower left')
-	fig.suptitle(f"$\Delta$T for {nDeposits} deposits, Position of deposits, Laser profile")
-	#plt.title(f"$\Delta$T for {nDeposits} deposits")
-	
+	fig.suptitle(f"Silica microparticle temperature gradient")
+
 	
 	#Colorbar & imshow
-	im = ax[0].imshow(H.T, origin='lower',  cmap='plasma',
-            extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]])
-	cbar = plt.colorbar(im,ax=ax[0])
-	cbar.set_label(f"$\Delta$T")
 	
-	X,Y,Z = generateLaserProfile(spatialPeriodicity)
-	ax[2].contourf(X,Y,Z,50,cmap="gray")
+	
+
 	
 
 #Main() below:
@@ -115,7 +124,9 @@ else:
 tic = time.time()
 
 x,y,z,grad,depositDF = loadData()
-
+x_bins = np.linspace(-imageBounds,imageBounds,200)
+y_bins = np.linspace(-imageBounds,imageBounds,200)
+H, xedges, yedges = histogram2d_cython(x, z, grad, x_bins, y_bins)
 generateFigure(imageBounds)
 
 os.chdir("..")
