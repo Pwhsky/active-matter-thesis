2b6bb12df8bb95aadffc57b0981ee6c490620638
Pwhsky <lechal.pierre@gmail.com>
2024-03-21 15:36:37 +0100

    More data analysis, polyfit works and now gives velocity

--- thermophoresis/time_evolution.py
+++ thermophoresis/time_evolution.py
@@ -40,7 +40,7 @@ def compute_MSD(x):
 
 
 def generateFigure():
-	makeFigure = True
+	makeFigure = False
 	if makeFigure:
 		fig, ax     = plt.subplots(1, 2, figsize=(10, 7))
 		xlim = [- limit,  limit]
@@ -59,8 +59,8 @@ def generateFigure():
 
 		#Generate contour plot of laser profile:
 
-		ax[0].contourf(X,Y,Z2,400,alpha=1,zorder=-1)
-		ax[1].contourf(X,Y,Z1,400,alpha=1,zorder=-1)
+		ax[0].contourf(X,Y,Z,400,alpha=1,zorder=-1)
+		ax[1].contourf(X,Y,Z,400,alpha=1,zorder=-1)
 		######################################
 
 		#Plot, place, and draw the deposits, circles, trajectory:
@@ -99,76 +99,76 @@ def generateFigure():
 		os.chdir("..")
 		os.chdir("src")
 ##################################################################################################
+	
+
 	fig, ax     = plt.subplots(3, 1, figsize=(17, 7))
 	fig.suptitle("Particle displacement",fontsize=20)
 	ylabels = ["X","Y","Z"]
 
-	ax[0].plot(trajectory[:,0],trajectory[:,1],label="X displacement",linewidth=1.5)
-	ax[1].plot(trajectory[:,0],trajectory[:,2],label="Y displacement",linewidth=1.5)
-	ax[2].plot(trajectory[:,0],trajectory[:,3],label="Z displacement",linewidth=1.5)
+	#ax[0].plot(trajectory[:,0],trajectory[:,1],label="X displacement",linewidth=1.5)
+	#ax[1].plot(trajectory[:,0],trajectory[:,2],label="Y displacement",linewidth=1.5)
+	#ax[2].plot(trajectory[:,0],trajectory[:,3],label="Z displacement",linewidth=1.5)
 
-	for i in range(3):
-		ax[i].set_xlabel("Time (s)",fontsize=18)
-		ax[i].set_ylabel(ylabels[i])
-		ax[i].legend()
-		ax[i].grid()
-		ax[i].tick_params(axis='both',which='major',labelsize=15)
+	#for i in range(3):
+	#	ax[i].set_xlabel("Time (s)",fontsize=18)
+	#	ax[i].set_ylabel(ylabels[i])
+	#	ax[i].legend()
+	#	ax[i].grid()
+	#	ax[i].tick_params(axis='both',which='major',labelsize=15)
 
 	os.chdir("..")
 	os.chdir("figures")
 	plt.savefig("displacement.png",facecolor=faceColor)
 	plt.clf()
 ######################################################################################################
-	fig.suptitle(f"Mean square displacement for Λ = {round(spatialPeriodicity*1000000,1)}µm ",fontsize=24)
 
+	fig.suptitle(f"Mean square displacement for Λ = {round(spatialPeriodicity*1000000,1)}µm ",fontsize=24)
+	legend_labels = ["X","Y","Z"]
 	plt.xlabel("t [s]",fontsize=20)
 	plt.ylabel(r"MSD(t) [µm²] ",fontsize=20)
 
-	#Rescale to be in units of micrometer
-	MSDZ = compute_MSD(trajectory[:,3])*1e12
-	MSDY = compute_MSD(trajectory[:,2])*1e12
-	MSDX = compute_MSD(trajectory[:,1])*1e12
+	#MSD contains the mean square displacement in different axis[MSD_x, MSD_y, MSD_z]
 
-	plt.loglog(trajectory[2:,0],MSDZ[2:],        label = "Z",linewidth=3)
-	plt.loglog(trajectory[2:-100,0],MSDX[2:-100],label = "X",linewidth=3)
-	plt.loglog(trajectory[2:-100,0],MSDY[2:-100],label = "Y",linewidth=3)
+	#Plot the MSD:
+	t = trajectory[:,0]
+	plt.xlim([t[1],np.max(t)])
+	for i in range(3):
+		plt.loglog(trajectory[1:,0],MSD[i][1:],label=legend_labels[i],linewidth=3)
 
 
-	
-	slope_z = np.asarray( [  np.log(MSDZ[20]/MSDZ[1])  /np.log(20), 
-							 np.log(MSDZ[400]/MSDZ[20])/np.log(380/20),
-							 np.log(MSDZ[-1]/MSDZ[400])/np.log(len(MSDZ[400:])/380 )  ])
+	#Method 1 to obtain slope of MSD, divide MSD with t^2
+	#the slopes are in 3 regions of the plot, t[0:20] t[20:400] etc 
+	slope_x = np.asarray( [  np.log(MSD[0][20]/MSD[0][1])    /np.log(20), 
+							 np.log(MSD[0][400]/MSD[0][20])  /np.log(380/20),
+							 np.log(MSD[0][-500]/MSD[0][400])/np.log((len(MSD[0][400:])-500)/380 )  ])
+	slope_y = np.asarray( [  np.log(MSD[1][20]/MSD[1][1])    /np.log(20), 
+							 np.log(MSD[1][400]/MSD[1][20])  /np.log(380/20),
+							 np.log(MSD[1][-500]/MSD[1][400])/np.log((len(MSD[1][400:])-500)/380 )  ])
+	slope_z = np.asarray( [  np.log(MSD[2][20]/MSD[2][1])  /np.log(20), 
+							 np.log(MSD[2][400]/MSD[2][20])/np.log(380/20),
+							 np.log(MSD[2][-1]/MSD[2][400])/np.log(len(MSD[2][400:])/380 )  ])
 
-	slope_y = np.asarray( [  np.log(MSDY[20]/MSDY[1])    /np.log(20), 
-							 np.log(MSDY[400]/MSDY[20])  /np.log(380/20),
-							 np.log(MSDY[-500]/MSDY[400])/np.log((len(MSDY[400:])-500)/380 )  ])
+	#Method 2, perform logarithm polyfit (with error estimate):
+	x = trajectory[100:-1,0]
+	coefficients, covariance = np.polyfit(np.log(x),np.log(np.squeeze(MSD[2][100:-1])),1,cov=True)
+	errors                   = np.sqrt(np.diag(covariance))
 
-	slope_x = np.asarray( [  np.log(MSDX[20]/MSDX[1])    /np.log(20), 
-							 np.log(MSDX[400]/MSDX[20])  /np.log(380/20),
-							 np.log(MSDX[-500]/MSDX[400])/np.log((len(MSDX[400:])-500)/380 )  ])
+	#from b = 2*log(v) where v is velocity, we can derive velocity with error bars
+	#a and b come from coefficients from numpy polyfit.
+	#coefficients[0] = 2
+	#print(coefficients)
+	#print(errors)
 
+	velocity       = np.exp((coefficients[0])/2)
+	velocity_error = np.exp((coefficients[0]+errors[0])/2)-np.exp((coefficients[0]-errors[0])/2)
+
+	print(f"velocity = {round(velocity,5)} ± {round(velocity_error,5)}" )
+	plt.loglog(t,np.exp(np.polyval(coefficients,np.log(t))),'--',label = 
+						 f"exp({round(coefficients[0],2)}log(t) {round(coefficients[1],2)})")
+
+
+	#plt.loglog(t,np.log(slope_z[2])*t**slope_z[2],'--',label = "fit")
 	printSlopes = False
-	if printSlopes:
-		print("Slopes of Z:")
-		print(slope_z)
-		print("Slopes of Y:")
-		print(slope_y)
-		print("Slopes of X:")
-		print(slope_x)
-	
-	num = 1000
-	velocities =  [np.divide(MSDX[num:-100],(trajectory[num:-100,0]**2)),
-				   np.divide(MSDY[num:-100],(trajectory[num:-100,0]**2)),
-				   np.divide(MSDZ[num:-100],(trajectory[num:-100,0]**2))]
-
-	#Unit of v_z is meter per millisecond (dt = 0.01)
-	#Rescale to micrometers per second: 
-	print("V_x,V_y,V_z")
-	for vel in velocities:
-		print(np.mean(vel))
-	print("err_x, err_y, err_z")	
-	for vel in velocities:
-		print(np.std(vel)/np.sqrt((len(MSDX[num:-100]))))
 
 	plt.tick_params(axis='both', which='major', labelsize=20)
 	plt.tick_params(axis='both', which='minor', labelsize=20)
@@ -202,23 +202,27 @@ def update(frame):
 x   = np.linspace(-spatialPeriodicity*20,spatialPeriodicity*20, 200)
 y   = np.linspace(-spatialPeriodicity*20,spatialPeriodicity*20, 200)
 X,Y = np.meshgrid(x,y)	
-Z1 = (1+ (np.cos(2*pi*((X))/(spatialPeriodicity))))
-Z2 = (1+ (np.cos(2*pi*(np.sqrt(X**2 + Y**2))/(spatialPeriodicity))))
+Z = (1+ (np.cos(2*pi*((X))/(spatialPeriodicity))))
+
 
 ################# C++ SIMULATION, YOU CAN REUSE OLD DATA TO SAVE TIME
-generateNewData()
+#generateNewData()
 ##################
 
 #Trajectory.csv contains time,x,y,z,velocity
-trajectory = np.genfromtxt("trajectory.csv",delimiter=',',skip_header=1)
+trajectory = np.genfromtxt("trajectory.csv",delimiter=',',skip_header=3)
 deposits   = np.genfromtxt("deposits.csv",delimiter=',',skip_header=1)
 N = len(trajectory)
-
 x_positions = trajectory[:,1]
 y_positions = trajectory[:,2]
-
 x_deposits  = deposits[:, 0]
 y_deposits  = deposits[:, 1]
+
+	#Rescale to be in units of micrometers squared
+MSD = np.asarray([compute_MSD(trajectory[:,1])*1e12, 
+				  compute_MSD(trajectory[:,2])*1e12,
+				  compute_MSD(trajectory[:,3])*1e12	])
+
 
 print("Computing MSD...")
 ################ GRAPHS AND FIGURES
@@ -231,8 +235,8 @@ sc     = ax.scatter([], [], color='b', edgecolor='k', marker='o', facecolor='non
 tic    = time.time()
 
 ################ MOVIE, TAKES A LONG TIME
-ani = animation.FuncAnimation(fig, update, frames=N, interval=50,blit=True)
-ani.save('particle_animation.mp4', writer='ffmpeg')
+#ani = animation.FuncAnimation(fig, update, frames=N, interval=50,blit=True)
+#ani.save('particle_animation.mp4', writer='ffmpeg')
 ################
 
 toc = time.time()
